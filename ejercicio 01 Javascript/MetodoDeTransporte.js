a0bdef39648aa65a64039f43eaf0b86d852f4788
JCBTK <59676003+JCBTK@users.noreply.github.com>
2023-03-29 16:51:30 -0300

    agregados los archivos,

---
+++ ejercicio 01 Javascript/MetodoDeTransporte.js
@@ -0,0 +1,17 @@
+function Transporte(Distancia) {
+  if (Distancia >= 0 && Distancia < 1000) {
+    return "A pie";
+  } else if (Distancia >= 1000 && Distancia < 10000) {
+    return "Bicicleta";
+  } else if (Distancia >= 10000 && Distancia < 30000) {
+    return "Colectivo";
+  } else if (Distancia >= 30000 && Distancia < 100000) {
+    return "Auto";
+  } else if (Distancia >= 100000) {
+    return "Avion";
+  } else {
+    return "Distancia no valida";
+  }
+}
+
+  
